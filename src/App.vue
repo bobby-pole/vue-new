9f287b792d945449af7ac2bbe596f9a73e7ca97b
robertlacheta <88884140+robertlacheta@users.noreply.github.com>
2022-02-20 13:33:37 +0100

    added reaction timer game

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
-  <teleport to=".modals" v-if="showModal">
+  <teleport to="#modals" v-if="showModal">
     <Modal theme="" v-on:close="toggleModal">
       <template v-slot:links>
         <a href="#">sign up now</a>
@@ -12,28 +12,40 @@
     </Modal>
   </teleport>
 
-  <teleport to=".modals" v-if="showModalTwo">
-    <Modal v-on:close="toggleModalTwo">
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
       <h1>Sign up to the newsletter</h1>
       <p>For updates and promo codes!</p>
     </Modal>
   </teleport>
 
-  <button v-on:click="toggleModal">open modal</button>
-  <button v-on:click="toggleModalTwo">open second modal</button>
+  <button @click="toggleModal">open modal</button>
+  <button @click="toggleModalTwo">open second modal</button>
+  <div>
+    <h1>Ninja Reaction Timer</h1>
+  </div>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Modal from "./components/Modal.vue";
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
 
 export default {
   name: "App",
-  components: { Modal },
+  components: { Modal, Block, Results },
   data() {
     return {
       title: "My first Vue App :)",
       showModal: false,
       showModalTwo: false,
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
     };
   },
   methods: {
@@ -43,12 +55,23 @@ export default {
     toggleModalTwo() {
       this.showModalTwo = !this.showModalTwo;
     },
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
   },
 };
 </script>
 
 <style>
-#app .modals {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -61,5 +84,21 @@ h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
+}
+
+button {
+  background-color: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
